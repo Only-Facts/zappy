cb848d9015ff33f12b6e0a381b16705aa5910bcc
YetAnotherMechanicusEnjoyer <alrawiahmed006@gmail.com>
2026-06-27 13:19:06 +0200

    Delete poc/libs/zig_mandel directory

--- poc/libs/zig_mandel/stdlib.h
+++
@@ -1,1 +0,0 @@
-typedef unsigned long size_t; void free(void *ptr); void *realloc(void *ptr, size_t size); void abort(void);
--- poc/libs/zig_mandel/string.h
+++
@@ -1,1 +0,0 @@
-typedef unsigned long size_t; size_t strlen(const char *s); void *memcpy(void *dest, const void *src, size_t n);
